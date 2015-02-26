269bf65707d2a677502534f23327f690fc93cabd
Stefan Niederhauser <ghuder5@gmx.ch>
2015-02-26 03:27:40 +0100

    support ArrowFunctionExpression

--- src/codegeneration/ConditionTransformer.js
+++ src/codegeneration/ConditionTransformer.js
@@ -36,51 +36,58 @@ import {ParseTreeTransformer} from './ParseTreeTransformer.js';
 
 export class ConditionTransformer extends ParseTreeTransformer {
 
-  transformFunctionDeclaration(tree) {
-    tree.body.statements = createTargetStatements(...separateSourceStatements(tree.body.statements));
+  transformArrowFunctionExpression(tree) {
+    if (tree.body.statements) {
+      tree.body.statements = this.createTargetStatements(...this.separateSourceStatements(tree.body.statements));
+    }
     return tree;
+  }
 
-    function separateSourceStatements(statements) {
-      var require, ensure;
-      let body = [];
-      for (let st of statements) {
-        if (st instanceof RequireStatement) {
-          require = st;
-        } else if (st instanceof EnsureStatement) {
-          ensure = st;
-        } else {
-          body.push(st);
-        }
-      }
-      return [require, ensure, body];
-    }
+  transformFunctionDeclaration(tree) {
+    tree.body.statements = this.createTargetStatements(...this.separateSourceStatements(tree.body.statements));
+    return tree;
+  }
 
-    function createTargetStatements(require, ensure, body) {
-      let checkRequire = createCheckBlock('Precondition', require);
-      if (!ensure) {
-        return [checkRequire, createBlock(body)];
+  separateSourceStatements(statements) {
+    var require, ensure;
+    let body = [];
+    for (let st of statements) {
+      if (st instanceof RequireStatement) {
+        require = st;
+      } else if (st instanceof EnsureStatement) {
+        ensure = st;
+      } else {
+        body.push(st);
       }
-      let callBody = createImmediatelyInvokedFunctionExpression(createFunctionBody(body)),
-        resultVar = createIdentifierExpression('$result'),
-        assignResult = createVariableStatement(createVariableDeclarationList(LET, resultVar, callBody)),
-        checkEnsure = createCheckBlock('Postcondition', ensure),
-        returnResult = createReturnStatement(resultVar);
-      return [checkRequire, assignResult, checkEnsure, returnResult];
     }
+    return [require, ensure, body];
+  }
 
-    function createCheckBlock(name, statement) {
-      if (!statement) {
-        return createEmptyBlock();
-      }
-      let checks = [];
-      for (let arg of statement.args.args) {
-        let writer = new ParseTreeWriter();
-        writer.visitAny(arg);
-        checks.push(createIfStatement(createUnaryExpression(createOperatorToken(BANG), createParenExpression(arg)),
-          createThrowStatement(createNewExpression(
-            createIdentifierExpression('Error'), createArgumentList([createStringLiteral(name + ' violated: ' + writer.toString())])))));
-      }
-      return createBlock(checks);
+  createTargetStatements(require, ensure, body) {
+    let checkRequire = this.createCheckBlock('Precondition', require);
+    if (!ensure) {
+      return [checkRequire, createBlock(body)];
     }
+    let callBody = createImmediatelyInvokedFunctionExpression(createFunctionBody(body)),
+      resultVar = createIdentifierExpression('$result'),
+      assignResult = createVariableStatement(createVariableDeclarationList(LET, resultVar, callBody)),
+      checkEnsure = this.createCheckBlock('Postcondition', ensure),
+      returnResult = createReturnStatement(resultVar);
+    return [checkRequire, assignResult, checkEnsure, returnResult];
+  }
+
+  createCheckBlock(name, statement) {
+    if (!statement) {
+      return createEmptyBlock();
+    }
+    let checks = [];
+    for (let arg of statement.args.args) {
+      let writer = new ParseTreeWriter();
+      writer.visitAny(arg);
+      checks.push(createIfStatement(createUnaryExpression(createOperatorToken(BANG), createParenExpression(arg)),
+        createThrowStatement(createNewExpression(
+          createIdentifierExpression('Error'), createArgumentList([createStringLiteral(name + ' violated: ' + writer.toString())])))));
+    }
+    return createBlock(checks);
   }
 }
