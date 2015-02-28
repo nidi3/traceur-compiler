b8994209997cc8677c5afcf481062bd401efd07f
Stefan Niederhauser <ghuder5@gmx.ch>
2015-02-28 02:40:14 +0100

    add $old

--- src/codegeneration/ConditionTransformer.js
+++ src/codegeneration/ConditionTransformer.js
@@ -5,9 +5,15 @@ import {
   CallExpression,
   ExpressionStatement,
   EnsureStatement,
-  RequireStatement
+  RequireStatement,
+  ThisExpression,
+  MemberExpression,
   } from '../syntax/trees/ParseTrees.js';
-import{  FUNCTION_EXPRESSION,FOR_STATEMENT,ARRAY_LITERAL_EXPRESSION} from '../syntax/trees/ParseTreeType.js';
+import{  FUNCTION_EXPRESSION,FOR_STATEMENT,ARRAY_LITERAL_EXPRESSION,MEMBER_EXPRESSION,IDENTIFIER_EXPRESSION} from '../syntax/trees/ParseTreeType.js';
+import {  BANG,LET,VAR,EQUAL_EQUAL_EQUAL,TYPEOF,AND,IDENTIFIER,THIS  } from '../syntax/TokenType.js';
+import {IdentifierToken} from '../syntax/IdentifierToken.js';
+import {ParseTreeVisitor} from '../syntax/ParseTreeVisitor.js';
+
 import {
   createIfStatement,
   createThrowStatement,
@@ -35,9 +41,12 @@ import {
   createMemberExpression,
   createThisExpression,
   createCallExpression,
-  createScopedExpression
+  createScopedExpression,
+  createObjectLiteral,
+  createObjectLiteralExpression,
+  createPropertyNameAssignment,
+  createLiteralPropertyName,
   } from './ParseTreeFactory.js';
-import {  BANG,LET,VAR,EQUAL_EQUAL_EQUAL,TYPEOF,AND  } from '../syntax/TokenType.js';
 import {  ParseTreeWriter  } from '../outputgeneration/ParseTreeWriter.js';
 import {ParseTreeTransformer} from './ParseTreeTransformer.js';
 
@@ -81,6 +90,16 @@ export class ConditionTransformer extends ParseTreeTransformer {
     let res = [];
     this.moveRestArgumentsUp_(body, res);
 
+    if (ensure) {
+      let orf = new OldReferenceFinder();
+      for (let e of ensure.args.args) {
+        orf.visitAny(e);
+      }
+      let old = orf.getObjectLiteral();
+      if (old.propertyNameAndValues.length > 0) {
+        res.push(createVariableStatement(createVariableDeclarationList(VAR, createIdentifierExpression('$old'), old)));
+      }
+    }
     if (require) {
       res.push(createBlock(this.createCheckStatements('Precondition', require)));
     }
@@ -156,6 +175,79 @@ class ArgumentsRenamer extends ParseTreeTransformer {
       this.changed = true;
     }
     return tree;
+  }
+}
+
+class OldReferenceFinder extends ParseTreeVisitor {
+  constructor() {
+    this.refs_ = {};
+  }
+
+  visitMemberExpression(tree) {
+    let lin = this.linearize_(tree);
+    if (lin && lin[0] === '$old') {
+      this.addRef_(lin.slice(1));
+    }
+  }
+
+  addRef_(list) {
+    var r = this.refs_;
+    var i = 0;
+    for (; i < list.length - 1; i++) {
+      let elem = list[i];
+      if (!r[elem]) {
+        r[elem] = {};
+      }
+      r = r[elem];
+    }
+    r[list[i]] = list.length === 1 ? this.createIdentifierExpression_(list[0]) : this.createMemberExpression_.apply(this, list);
+  }
+
+  createIdentifierExpression_(name) {
+    return name === THIS ? new ThisExpression(null) : new IdentifierExpression(null, name);
+  }
+
+  createMemberExpression_(operand, memberName, memberNames) {
+    if (typeof operand === 'string' || operand instanceof IdentifierToken)
+      operand = this.createIdentifierExpression_(operand);
+    if (typeof memberName === 'string')
+      memberName = createIdentifierToken(memberName);
+    let tree = new MemberExpression(null, operand, memberName);
+    for (let i = 2; i < arguments.length; i++) {
+      tree = this.createMemberExpression_(tree, arguments[i]);
+    }
+    return tree;
+  }
+
+  getObjectLiteral() {
+    function objectLiteral(obj) {
+      let ass = [];
+      for (let name in obj) {
+        let value = obj[name];
+        let v = value.type ? value : objectLiteral(value);
+        ass.push(createPropertyNameAssignment(createLiteralPropertyName(name), v));
+      }
+      return createObjectLiteralExpression(ass);
+    }
+
+    return objectLiteral(this.refs_);
+  }
+
+  linearize_(tree) {
+    let idents = [];
+    var t = tree;
+    while (t.type === MEMBER_EXPRESSION) {
+      if (t.memberName.type !== IDENTIFIER) {
+        return null;
+      }
+      idents.unshift(t.memberName.value);
+      t = t.operand;
+    }
+    if (t.type !== IDENTIFIER_EXPRESSION) {
+      return null;
+    }
+    idents.unshift(t.identifierToken.value);
+    return idents;
   }
 }
 
